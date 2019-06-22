5bb066bf21c97b0f40a5a25d8fe694bfb7f9d140
jingzhongzhi <jingzhongzhi@meituan.com>
2019-06-22 18:07:12 +0800

    添加注释

--- flink-oe/src/main/java/com/meituan/flink/VirtualHighMonitorJob.java
+++ flink-oe/src/main/java/com/meituan/flink/VirtualHighMonitorJob.java
@@ -12,7 +12,7 @@ import com.meituan.flink.qualitycontrol.VirtualHighKeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumerBase;
 
@@ -41,7 +41,8 @@ public class VirtualHighMonitorJob {
         DataStream<QualityControlResultMq> jsonData = source.rebalance().map(new QcJsonDataParse()).name("2. parse json data");
         DataStream<QualityControlResultMq> filterData = jsonData.filter(o -> o != null && o.getClientAppKey() != null).uid("3. filter null data").name("3. filter null data");
 
-        DataStream<WC> source2 = filterData.keyBy(new VirtualHighKeySelector()).window(TumblingProcessingTimeWindows.of(Time.seconds(60))).apply((new CounterWindow())).name("4. sum data by client appkey");
+        //使用 apply 的方式计算总数，是在窗口最后才计算，存储的是明细数据
+        DataStream<WC> source2 = filterData.keyBy(new VirtualHighKeySelector()).window(SlidingProcessingTimeWindows.of(Time.seconds(60), Time.seconds(5))).apply((new CounterWindow())).name("4. sum data by client appkey");
         source2.addSink(new SinkConsole()).name("5. sink to console");
         env.execute((new JobConf(args)).getJobName());
     }
