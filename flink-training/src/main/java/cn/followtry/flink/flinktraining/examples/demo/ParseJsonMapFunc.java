733b308a9f3898c5abb6bf5ac4ba9df6d4ffa32d
jingzhongzhi <jingzhongzhi@meituan.com>
2019-07-02 19:30:40 +0800

    添加 counter 指标监控

--- flink-training/src/main/java/cn/followtry/flink/flinktraining/examples/demo/ParseJsonMapFunc.java
+++ flink-training/src/main/java/cn/followtry/flink/flinktraining/examples/demo/ParseJsonMapFunc.java
@@ -3,6 +3,8 @@ package cn.followtry.flink.flinktraining.examples.demo;
 import cn.followtry.app.UserInfo;
 import com.alibaba.fastjson.JSON;
 import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.metrics.Counter;
 
 /**
  * @author jingzhongzhi
@@ -10,8 +12,17 @@ import org.apache.flink.api.common.functions.RichMapFunction;
  * @since 2019/6/27
  */
 public class ParseJsonMapFunc extends RichMapFunction<String, UserInfo> {
+
+    private transient Counter counter;
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+        counter = this.getRuntimeContext().getMetricGroup().counter("ParseJsonMapFunc");
+    }
+
     @Override
     public UserInfo map(String value) throws Exception {
+        this.counter.inc();
         UserInfo userInfo = null;
         try {
             userInfo = JSON.parseObject(value, UserInfo.class);
