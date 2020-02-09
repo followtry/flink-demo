59afa5ea2c7a3badc3ea70802e1f4881483e6e57
jingzhongzhi <jingzhongzhi@meituan.com>
2020-02-10 00:03:36 +0800

    feat（ stream ）:  sink result to socket to replace to std out

--- flink-demo/src/main/java/stream/StreamExample.java
+++ flink-demo/src/main/java/stream/StreamExample.java
@@ -2,6 +2,7 @@ package stream;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.serialization.SerializationSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.LocalStreamEnvironment;
@@ -21,22 +22,37 @@ public class StreamExample {
     
     private static final Integer port = 10000;
     
+    private static final Integer port2 = 10001;
+    
     
     /**
+     * 接收 localhost:10000 的 socket 链接的消息。
+     *
+     * socket连接使用 nc -lk 10000 来启动
      * main.
      */
     public static void main(String[] args) throws Exception{
         
         LocalStreamEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
     
-        DataStream<Tuple2<String, Integer>> dataStream = env.socketTextStream(host, port)
+        DataStream<Tuple2<String, Integer>> dataStream =
+                env.socketTextStream(host, port)
                 .flatMap(new Splitter())
                 .keyBy(0)
                 .timeWindow(Time.seconds(5))
                 .sum(1);
         
-        dataStream.print();
+        //将结果打印到标准输出，控制台
+//        dataStream.print();
         
+        //将结果打印到 socket接收端
+        dataStream.writeToSocket(host, port2, new SerializationSchema<Tuple2<String, Integer>>() {
+            @Override
+            public byte[] serialize(Tuple2<String, Integer> element) {
+                String s = element.toString()+"\n";
+                return  s.getBytes();
+            }
+        });
         env.execute("word count example");
     }
     
